33d2ec6717df86163c5ffd8ce356a03f041f9e62
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-11 19:39:42 +0530

    Added:Length of a Line

--- src/com/bridgelabz/LineComparisonProblem/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem/LineComparisonProblem.java
@@ -4,5 +4,8 @@ public class LineComparisonProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		int x1 = 15, x2 = 150, y1 = 65, y2 = 200;
+		int length = (int) Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Length of the line (" + x1 + ", " + x2 + ") and (" + y1 + ", " + y2 + ") is " + length);
 	}
 }
